528db25ec13d2d74e6070bfc0a220e78189e9c91
Shawn Weisfeld Build <sweisfel@microsoft.com>
2021-02-14 15:19:09 +0000

    autobuild

---
+++ docs/en.search.min.694abc53e342ebaf0e19a9dc260dda64ed3d743606d265abfae56858c3ca84c2.js
@@ -0,0 +1,11 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');if(!input){return}
+input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(input===document.activeElement){return;}
+const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
+input.focus();event.preventDefault();}
+function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.582cebc126a07c64f8faabf37428ba9a95306642f2c3e02027b1e9c3f82cd384.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}
+function element(content){const div=document.createElement('div');div.innerHTML=content;return div.firstChild;}})();
--- src/content/docs/whats-new/index.md
+++ src/content/docs/whats-new/index.md
@@ -6,6 +6,9 @@ title: "What's New"
 
 - [What are the differences between the Azure Blob Storage Upload API’s and when should I use each?](https://azurestorage.com/docs/application-and-user-data/basics/azure-blob-storage-upload-apis/) (02/09/2021)
 - [ADLS Gen 1 to Gen 2 Migration Guide](https://azurestorage.com/docs/analytics/adls-gen1-to-gen2-migration/) (02/08/2021)
+- [NFS 3.0 support for Azure Blob Storage](https://azurestorage.com/docs/application-and-user-data/basics/nfs-3-support-for-azure-blob-storage/) (02/03/2021)
+- [Optimize your costs with Azure Blob Storage](https://azurestorage.com/docs/application-and-user-data/basics/optimize-your-costs-with-azure-blob-storage/) (02/01/2021)
+- [Azure Blob Storage data protection features](https://azurestorage.com/docs/application-and-user-data/basics/azure-blob-storage-data-protection-features/) (01/28/2021)
 - [Managing concurrent uploads with versioning](https://azurestorage.com/docs/application-and-user-data/code-samples/concurrent-uploads-with-versioning/) (01/25/2021)
 - [AzReplicate](https://aka.ms/AzReplicate) (08/30/2020)
 - [AzDataMaker](https://aka.ms/AzDataMaker) (08/26/2020)
